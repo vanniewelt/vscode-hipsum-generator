295bdf28a38ed4eb72748456375b0615c119b2ca
Ondřej Nývlt <7478957+vanniewelt@users.noreply.github.com>
2021-07-22 23:47:12 +0200

    Implement extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,135 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import axios from "axios";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hipsum-generator" is now active!');
+const URL = "http://hipsum.co/api/";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hipsum-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hipster ipsum generator!');
-	});
-
-	context.subscriptions.push(disposable);
+interface HipsumParams {
+  type: "hipster-centric" | "hipster-latin";
+  paras?: number;
+  sentences?: number;
+  startWithLorem?: boolean;
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function isStringArray(stuff: unknown): stuff is Array<string> {
+  return (
+    Array.isArray(stuff) &&
+    stuff.every((paragraph) => typeof paragraph === "string")
+  );
+}
+
+async function fetchData({
+  type,
+  paras,
+  sentences,
+  startWithLorem,
+}: HipsumParams): Promise<Array<string>> {
+  const data: unknown = await axios
+    .get(URL, {
+      params: {
+        type,
+        paras,
+        sentences,
+        "start-with-lorem": startWithLorem,
+      },
+    })
+    .then((res) => res.data);
+
+  if (!isStringArray(data)) {
+    throw new Error("Invalid response");
+  }
+
+  return data;
+}
+
+async function getSentencesOrParagraphs(): Promise<
+  "sentences" | "paragraphs" | undefined
+> {
+  const input = await vscode.window.showQuickPick<
+    vscode.QuickPickItem & { id: "sentences" | "paragraphs" }
+  >(
+    [
+      { id: "paragraphs", label: "Paragraphs" },
+      { id: "sentences", label: "Sentences" },
+    ],
+    {
+      title: "Do you want to insert a number of sentences or paragraphs?",
+    }
+  );
+
+  return input?.id;
+}
+
+async function getNumber(
+  sentencesOrParagraphs: "sentences" | "paragraphs"
+): Promise<number | undefined> {
+  const input = await vscode.window.showInputBox({
+    title: `Enter number of ${sentencesOrParagraphs}`,
+    value: "1",
+    validateInput: (value) => {
+      return isNaN(parseInt(value)) ? "Value must be an integer" : null;
+    },
+  });
+
+  if (!input) {
+    return;
+  }
+
+  return parseInt(input);
+}
+
+async function run(editor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+  const sentencesOrParapgraphs = await getSentencesOrParagraphs();
+
+  if (!sentencesOrParapgraphs) {
+    return;
+  }
+
+  const number = await getNumber(sentencesOrParapgraphs);
+
+  if (!number) {
+    return;
+  }
+
+  vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Window,
+      cancellable: false,
+      title: "Fetching hipster ipsum...",
+    },
+    async (progress) => {
+      progress.report({ increment: 0 });
+
+      const data = await fetchData({
+        type: "hipster-centric",
+        [sentencesOrParapgraphs === "paragraphs" ? "paras" : "sentences"]:
+          number,
+      });
+
+      progress.report({ increment: 100 });
+
+      const processedString = data
+        .join("\n\n") // Convert array of strings to string of double-newline separated paragraphs.
+        .replace("  ", " "); // hipsum.co API separates sentences with two spaces for some reason.
+
+      editor.edit((builder) => {
+        builder.replace(editor.selection, processedString);
+      });
+    }
+  );
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerTextEditorCommand(
+    "hipsum-generator.generate",
+    (editor, edit) => run(editor, edit)
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+  //
+}
